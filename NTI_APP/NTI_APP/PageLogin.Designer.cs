4b5ad3d4a16d9c099c98e123fdaeb394fbdb6e33
Максим Попов <max.popov.02@mail.ru>
2021-02-11 14:36:22 +0300

    Hot buhfix 7

--- NTI_APP/NTI_APP/PageLogin.Designer.cs
+++ NTI_APP/NTI_APP/PageLogin.Designer.cs
@@ -36,16 +36,16 @@ namespace NTI_APP
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageLogin));
             this.EnteryLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.EnteryBatton = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.EnteryButton = new System.Windows.Forms.Button();
+            this.panelPassword = new System.Windows.Forms.Panel();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.linkRegistration = new System.Windows.Forms.LinkLabel();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelMail = new System.Windows.Forms.Panel();
             this.textBoxEmail = new System.Windows.Forms.TextBox();
-            this.linkWastePassword = new System.Windows.Forms.LinkLabel();
+            this.linkForgotPassword = new System.Windows.Forms.LinkLabel();
             this.tableLayoutPanel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel1.SuspendLayout();
+            this.panelPassword.SuspendLayout();
+            this.panelMail.SuspendLayout();
             this.SuspendLayout();
             // 
             // EnteryLabel
@@ -66,12 +66,12 @@ namespace NTI_APP
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.EnteryBatton, 1, 3);
-            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.EnteryButton, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.panelPassword, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.EnteryLabel, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.linkRegistration, 0, 4);
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.linkWastePassword, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.panelMail, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.linkForgotPassword, 0, 3);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -84,37 +84,37 @@ namespace NTI_APP
             this.tableLayoutPanel1.Size = new System.Drawing.Size(424, 290);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
-            // EnteryBatton
+            // EnteryButton
             // 
-            this.EnteryBatton.BackColor = System.Drawing.Color.White;
-            this.EnteryBatton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("EnteryBatton.BackgroundImage")));
-            this.EnteryBatton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.EnteryBatton.Cursor = System.Windows.Forms.Cursors.Default;
-            this.EnteryBatton.Dock = System.Windows.Forms.DockStyle.Right;
-            this.EnteryBatton.FlatAppearance.BorderSize = 0;
-            this.EnteryBatton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
-            this.EnteryBatton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.EnteryBatton.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.EnteryBatton.Location = new System.Drawing.Point(326, 216);
-            this.EnteryBatton.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
-            this.EnteryBatton.Name = "EnteryBatton";
-            this.tableLayoutPanel1.SetRowSpan(this.EnteryBatton, 2);
-            this.EnteryBatton.Size = new System.Drawing.Size(95, 71);
-            this.EnteryBatton.TabIndex = 2;
-            this.EnteryBatton.UseVisualStyleBackColor = false;
+            this.EnteryButton.BackColor = System.Drawing.Color.White;
+            this.EnteryButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("EnteryButton.BackgroundImage")));
+            this.EnteryButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.EnteryButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.EnteryButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.EnteryButton.FlatAppearance.BorderSize = 0;
+            this.EnteryButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.EnteryButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EnteryButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.EnteryButton.Location = new System.Drawing.Point(326, 216);
+            this.EnteryButton.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.EnteryButton.Name = "EnteryButton";
+            this.tableLayoutPanel1.SetRowSpan(this.EnteryButton, 2);
+            this.EnteryButton.Size = new System.Drawing.Size(95, 71);
+            this.EnteryButton.TabIndex = 2;
+            this.EnteryButton.UseVisualStyleBackColor = false;
             // 
-            // panel2
+            // panelPassword
             // 
-            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
-            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.tableLayoutPanel1.SetColumnSpan(this.panel2, 2);
-            this.panel2.Controls.Add(this.textBoxPassword);
-            this.panel2.Location = new System.Drawing.Point(3, 143);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(418, 64);
-            this.panel2.TabIndex = 2;
+            this.panelPassword.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.panelPassword.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelPassword.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelPassword.BackgroundImage")));
+            this.panelPassword.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.tableLayoutPanel1.SetColumnSpan(this.panelPassword, 2);
+            this.panelPassword.Controls.Add(this.textBoxPassword);
+            this.panelPassword.Location = new System.Drawing.Point(3, 143);
+            this.panelPassword.Name = "panelPassword";
+            this.panelPassword.Size = new System.Drawing.Size(418, 64);
+            this.panelPassword.TabIndex = 2;
             // 
             // textBoxPassword
             // 
@@ -143,18 +143,18 @@ namespace NTI_APP
             this.linkRegistration.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.linkRegistration.VisitedLinkColor = System.Drawing.Color.Black;
             // 
-            // panel1
+            // panelMail
             // 
-            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
-            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
-            this.panel1.Controls.Add(this.textBoxEmail);
-            this.panel1.Location = new System.Drawing.Point(3, 73);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(418, 63);
-            this.panel1.TabIndex = 3;
+            this.panelMail.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.panelMail.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelMail.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelMail.BackgroundImage")));
+            this.panelMail.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.tableLayoutPanel1.SetColumnSpan(this.panelMail, 2);
+            this.panelMail.Controls.Add(this.textBoxEmail);
+            this.panelMail.Location = new System.Drawing.Point(3, 73);
+            this.panelMail.Name = "panelMail";
+            this.panelMail.Size = new System.Drawing.Size(418, 63);
+            this.panelMail.TabIndex = 3;
             // 
             // textBoxEmail
             // 
@@ -168,22 +168,22 @@ namespace NTI_APP
             this.textBoxEmail.Size = new System.Drawing.Size(360, 45);
             this.textBoxEmail.TabIndex = 2;
             // 
-            // linkWastePassword
+            // linkForgotPassword
             // 
-            this.linkWastePassword.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
-            this.linkWastePassword.AutoSize = true;
-            this.linkWastePassword.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.linkWastePassword.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
-            this.linkWastePassword.LinkColor = System.Drawing.Color.Black;
-            this.linkWastePassword.Location = new System.Drawing.Point(3, 210);
-            this.linkWastePassword.Name = "linkWastePassword";
-            this.linkWastePassword.Size = new System.Drawing.Size(206, 40);
-            this.linkWastePassword.TabIndex = 4;
-            this.linkWastePassword.TabStop = true;
-            this.linkWastePassword.Text = "ЗАБЫЛИ ПАРОЛЬ?";
-            this.linkWastePassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.linkWastePassword.VisitedLinkColor = System.Drawing.Color.Black;
-            this.linkWastePassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkWastePassword_LinkClicked);
+            this.linkForgotPassword.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            this.linkForgotPassword.AutoSize = true;
+            this.linkForgotPassword.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.linkForgotPassword.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.linkForgotPassword.LinkColor = System.Drawing.Color.Black;
+            this.linkForgotPassword.Location = new System.Drawing.Point(3, 210);
+            this.linkForgotPassword.Name = "linkForgotPassword";
+            this.linkForgotPassword.Size = new System.Drawing.Size(206, 40);
+            this.linkForgotPassword.TabIndex = 4;
+            this.linkForgotPassword.TabStop = true;
+            this.linkForgotPassword.Text = "ЗАБЫЛИ ПАРОЛЬ?";
+            this.linkForgotPassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.linkForgotPassword.VisitedLinkColor = System.Drawing.Color.Black;
+            this.linkForgotPassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkWastePassword_LinkClicked);
             // 
             // PageLogin
             // 
@@ -197,10 +197,10 @@ namespace NTI_APP
             this.Load += new System.EventHandler(this.UserControl1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.panelPassword.ResumeLayout(false);
+            this.panelPassword.PerformLayout();
+            this.panelMail.ResumeLayout(false);
+            this.panelMail.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -210,11 +210,11 @@ namespace NTI_APP
         private System.Windows.Forms.Label EnteryLabel;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TextBox textBoxPassword;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelMail;
         private System.Windows.Forms.TextBox textBoxEmail;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button EnteryBatton;
-        private System.Windows.Forms.LinkLabel linkWastePassword;
+        private System.Windows.Forms.Panel panelPassword;
+        private System.Windows.Forms.Button EnteryButton;
+        private System.Windows.Forms.LinkLabel linkForgotPassword;
         private System.Windows.Forms.LinkLabel linkRegistration;
     }
 }
