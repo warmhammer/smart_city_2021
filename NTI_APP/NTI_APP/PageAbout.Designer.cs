8fdf0a2268de3f10023813b20fbbad656f686cf2
Максим Попов <max.popov.02@mail.ru>
2021-02-11 19:43:02 +0300

    Mail-App opening test commit

--- NTI_APP/NTI_APP/PageAbout.Designer.cs
+++ NTI_APP/NTI_APP/PageAbout.Designer.cs
@@ -43,10 +43,10 @@ namespace NTI_APP
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
+            this.labelMail1 = new System.Windows.Forms.Label();
+            this.labelMail2 = new System.Windows.Forms.Label();
+            this.labelAddress1 = new System.Windows.Forms.Label();
+            this.labelAddress2 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -114,11 +114,11 @@ namespace NTI_APP
             this.tableLayoutPanel2.Controls.Add(this.label3, 1, 8);
             this.tableLayoutPanel2.Controls.Add(this.label4, 1, 1);
             this.tableLayoutPanel2.Controls.Add(this.label5, 1, 2);
-            this.tableLayoutPanel2.Controls.Add(this.label6, 1, 5);
-            this.tableLayoutPanel2.Controls.Add(this.label7, 1, 6);
-            this.tableLayoutPanel2.Controls.Add(this.label8, 1, 9);
-            this.tableLayoutPanel2.Controls.Add(this.label9, 1, 10);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(1184, 234);
+            this.tableLayoutPanel2.Controls.Add(this.labelMail1, 1, 5);
+            this.tableLayoutPanel2.Controls.Add(this.labelMail2, 1, 6);
+            this.tableLayoutPanel2.Controls.Add(this.labelAddress1, 1, 9);
+            this.tableLayoutPanel2.Controls.Add(this.labelAddress2, 1, 10);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1149, 234);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 12;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 65F));
@@ -133,7 +133,7 @@ namespace NTI_APP
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 65F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 65F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(580, 690);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(650, 690);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
             // panel2
@@ -145,7 +145,7 @@ namespace NTI_APP
             this.panel2.Margin = new System.Windows.Forms.Padding(0);
             this.panel2.Name = "panel2";
             this.tableLayoutPanel2.SetRowSpan(this.panel2, 2);
-            this.panel2.Size = new System.Drawing.Size(145, 130);
+            this.panel2.Size = new System.Drawing.Size(162, 130);
             this.panel2.TabIndex = 0;
             // 
             // panel3
@@ -157,7 +157,7 @@ namespace NTI_APP
             this.panel3.Margin = new System.Windows.Forms.Padding(0);
             this.panel3.Name = "panel3";
             this.tableLayoutPanel2.SetRowSpan(this.panel3, 2);
-            this.panel3.Size = new System.Drawing.Size(145, 130);
+            this.panel3.Size = new System.Drawing.Size(162, 130);
             this.panel3.TabIndex = 1;
             this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
@@ -170,50 +170,53 @@ namespace NTI_APP
             this.panel4.Margin = new System.Windows.Forms.Padding(0);
             this.panel4.Name = "panel4";
             this.tableLayoutPanel2.SetRowSpan(this.panel4, 2);
-            this.panel4.Size = new System.Drawing.Size(145, 130);
+            this.panel4.Size = new System.Drawing.Size(162, 130);
             this.panel4.TabIndex = 2;
             // 
             // label1
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(234, 13);
+            this.label1.Location = new System.Drawing.Point(165, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(256, 38);
+            this.label1.Size = new System.Drawing.Size(256, 65);
             this.label1.TabIndex = 3;
             this.label1.Text = "НАШИ ТЕЛЕФОНЫ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(262, 243);
+            this.label2.Location = new System.Drawing.Point(165, 230);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(201, 38);
+            this.label2.Size = new System.Drawing.Size(201, 65);
             this.label2.TabIndex = 4;
             this.label2.Text = "НАША ПОЧТА";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(256, 473);
+            this.label3.Location = new System.Drawing.Point(165, 460);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(213, 38);
+            this.label3.Size = new System.Drawing.Size(213, 65);
             this.label3.TabIndex = 5;
             this.label3.Text = "НАШИ АДРЕСА";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Dock = System.Windows.Forms.DockStyle.Left;
             this.label4.ForeColor = System.Drawing.Color.DimGray;
-            this.label4.Location = new System.Drawing.Point(148, 65);
+            this.label4.Location = new System.Drawing.Point(165, 65);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(192, 65);
             this.label4.TabIndex = 6;
@@ -224,55 +227,56 @@ namespace NTI_APP
             this.label5.AutoSize = true;
             this.label5.Dock = System.Windows.Forms.DockStyle.Left;
             this.label5.ForeColor = System.Drawing.Color.DimGray;
-            this.label5.Location = new System.Drawing.Point(148, 130);
+            this.label5.Location = new System.Drawing.Point(165, 130);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(342, 65);
             this.label5.TabIndex = 7;
             this.label5.Text = "Отдел по работе с подрядчиками:\r\n+7-(999)-999-99-99\r\n";
             // 
-            // label6
+            // labelMail1
             // 
-            this.label6.AutoSize = true;
-            this.label6.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label6.ForeColor = System.Drawing.Color.DimGray;
-            this.label6.Location = new System.Drawing.Point(148, 295);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(278, 65);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Отдел работы с клиентами:\r\ninfo@mail.ru\r\n";
+            this.labelMail1.AutoSize = true;
+            this.labelMail1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelMail1.ForeColor = System.Drawing.Color.DimGray;
+            this.labelMail1.Location = new System.Drawing.Point(165, 295);
+            this.labelMail1.Name = "labelMail1";
+            this.labelMail1.Size = new System.Drawing.Size(278, 65);
+            this.labelMail1.TabIndex = 8;
+            this.labelMail1.Text = "Отдел работы с клиентами:\r\ninfo@mail.ru\r\n";
+            this.labelMail1.Click += new System.EventHandler(this.label6_Click);
             // 
-            // label7
+            // labelMail2
             // 
-            this.label7.AutoSize = true;
-            this.label7.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label7.ForeColor = System.Drawing.Color.DimGray;
-            this.label7.Location = new System.Drawing.Point(148, 360);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(165, 65);
-            this.label7.TabIndex = 9;
-            this.label7.Text = "Оптовый отдел:\r\ninfo@mail.ru";
+            this.labelMail2.AutoSize = true;
+            this.labelMail2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelMail2.ForeColor = System.Drawing.Color.DimGray;
+            this.labelMail2.Location = new System.Drawing.Point(165, 360);
+            this.labelMail2.Name = "labelMail2";
+            this.labelMail2.Size = new System.Drawing.Size(165, 65);
+            this.labelMail2.TabIndex = 9;
+            this.labelMail2.Text = "Оптовый отдел:\r\ninfo@mail.ru";
             // 
-            // label8
+            // labelAddress1
             // 
-            this.label8.AutoSize = true;
-            this.label8.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label8.ForeColor = System.Drawing.Color.DimGray;
-            this.label8.Location = new System.Drawing.Point(148, 525);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(418, 65);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "Офис:\r\nг. Караганда, ул. Академика Пилюлькина, д.13";
+            this.labelAddress1.AutoSize = true;
+            this.labelAddress1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelAddress1.ForeColor = System.Drawing.Color.DimGray;
+            this.labelAddress1.Location = new System.Drawing.Point(165, 525);
+            this.labelAddress1.Name = "labelAddress1";
+            this.labelAddress1.Size = new System.Drawing.Size(456, 65);
+            this.labelAddress1.TabIndex = 10;
+            this.labelAddress1.Text = "Офис:\r\nг. Караганда, ул. Академика Пилюлькина, д.13";
             // 
-            // label9
+            // labelAddress2
             // 
-            this.label9.AutoSize = true;
-            this.label9.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label9.ForeColor = System.Drawing.Color.DimGray;
-            this.label9.Location = new System.Drawing.Point(148, 590);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(412, 65);
-            this.label9.TabIndex = 11;
-            this.label9.Text = "Склад:\r\nг. Карагадна, ул. Генерала Свистулькина, д.12\r\n";
+            this.labelAddress2.AutoSize = true;
+            this.labelAddress2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelAddress2.ForeColor = System.Drawing.Color.DimGray;
+            this.labelAddress2.Location = new System.Drawing.Point(165, 590);
+            this.labelAddress2.Name = "labelAddress2";
+            this.labelAddress2.Size = new System.Drawing.Size(450, 65);
+            this.labelAddress2.TabIndex = 11;
+            this.labelAddress2.Text = "Склад:\r\nг. Карагадна, ул. Генерала Свистулькина, д.12\r\n";
             // 
             // PageAbout
             // 
@@ -302,10 +306,10 @@ namespace NTI_APP
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelMail1;
+        private System.Windows.Forms.Label labelMail2;
+        private System.Windows.Forms.Label labelAddress1;
+        private System.Windows.Forms.Label labelAddress2;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label10;
     }
--- NTI_APP/NTI_APP/PageAbout.cs
+++ NTI_APP/NTI_APP/PageAbout.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Mail;
+using System.Reflection;
 
 namespace NTI_APP
 {
@@ -28,6 +32,54 @@ namespace NTI_APP
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            //var url = "mailto:someone@somewhere.com";
+            //Process.Start(url);
+            //"mailto:" + to + "&SUBJECT=" + subject + "?BODY=" + body + "&Attachment=" + attachment
+            //System.Diagnostics.Process.Start("mailto:bla@bla.bla");
+
+            //string subject = "Test subject";
+            //string emailTag = string.Format("mailto:someone@test.com?subject={0}", subject);
+            //System.Diagnostics.Process.Start(emailTag);
+
+            string target = "http://www.microsoft.com";
+            //Use no more than one assignment when you test this code.
+            //string target = "ftp://ftp.microsoft.com";
+            //string target = "C:\\Program Files\\Microsoft Visual Studio\\INSTALL.HTM";
+            try
+            {
+                System.Diagnostics.Process.Start(target);
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+
+            /*
+            var mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("someone@yourdomain.com");
+            mailMessage.Subject = "Your subject here";
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Body = "<span style='font-size: 12pt; color: red;'>My HTML formatted body</span>";
+
+            mailMessage.Attachments.Add(new Attachment("C://Myfile.pdf"));
+
+            var filename = "C://Temp/mymessage.eml";
+
+            //save the MailMessage to the filesystem
+            mailMessage.Save(filename);
+
+            //Open the file with the default associated application registered on the local machine
+            Process.Start(mailMessage);
+            */
         }
     }
 }
